d3bbde8307988e0ac8c9da3a50541bc5656a0299
Jim McTighe <jim.mctighe@bellmedia.ca>
2023-07-19 15:04:27 -0600

    added my project ID noble-history-393220

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "noble-history-393220" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
@@ -39,7 +39,7 @@ resource "google_compute_firewall" "default" {
 ## NGINX PROXY
 resource "google_compute_instance" "nginx_instance" {
   name         = "nginx-proxy"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   tags = ["web"]
   
   boot_disk {
@@ -60,7 +60,7 @@ resource "google_compute_instance" "nginx_instance" {
 ## WEB1
 resource "google_compute_instance" "web1" {
   name         = "web1"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   
   boot_disk {
     initialize_params {
@@ -77,7 +77,7 @@ resource "google_compute_instance" "web1" {
 ## WEB2
 resource "google_compute_instance" "web2" {
   name         = "web2"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   
   boot_disk {
     initialize_params {
@@ -93,7 +93,7 @@ resource "google_compute_instance" "web2" {
 ## WEB3
 resource "google_compute_instance" "web3" {
   name         = "web3"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   
   boot_disk {
     initialize_params {
@@ -110,7 +110,7 @@ resource "google_compute_instance" "web3" {
 ## DB
 resource "google_compute_instance" "mysqldb" {
   name         = "mysqldb"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   
   boot_disk {
     initialize_params {
